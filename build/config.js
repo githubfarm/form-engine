2c6e2b9ba6525e5ddf3b2eb07fa5668329511a0a
michael <github777@126.com>
2023-10-21 22:21:03 +0800

    docs: move code

--- build/config.js
+++ build/config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 exports.alias = {
-  main: path.resolve(__dirname, '../src'),
+  main: path.resolve(__dirname, '../document/common'),
   packages: path.resolve(__dirname, '../packages'),
   examples: path.resolve(__dirname, '../examples'),
   document: path.resolve(__dirname, '../document')
